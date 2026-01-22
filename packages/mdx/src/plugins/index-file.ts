71ff4282065aa702d2e1697bf1b4697823a9f024
Nishith Jain <kingnish24@gmail.com>
2026-01-22 20:23:30 +0530

    modified:   packages/mdx/src/plugins/index-file.ts

--- packages/mdx/src/plugins/index-file.ts
+++ packages/mdx/src/plugins/index-file.ts
@@ -5,7 +5,6 @@ import { type CodeGen, createCodegen, ident, slash } from '@/utils/codegen';
 import { glob } from 'tinyglobby';
 import { createFSCache } from '@/utils/fs-cache';
 import { createHash } from 'crypto';
-import type { LazyEntry } from '@/runtime/dynamic';
 import type { EmitEntry } from '@/core';
 import { fumaMatter } from '@/utils/fuma-matter';
 import type { ServerOptions } from '@/runtime/server';
@@ -147,8 +146,6 @@ export default function indexFile(options: IndexFilePluginOptions = {}): Plugin 
 
       const out: Promise<EmitEntry>[] = [toEmitEntry('server.ts', generateServerIndexFile)];
 
-      if (dynamic) out.push(toEmitEntry('dynamic.ts', generateDynamicIndexFile));
-
       if (browser) out.push(toEmitEntry('browser.ts', generateBrowserIndexFile));
 
       return await Promise.all(out);
@@ -212,93 +209,6 @@ async function generateServerIndexFile(ctx: FileGenContext) {
           collection,
           true,
         )})`;
-    }
-  }
-
-  await codegen.pushAsync(
-    core.getCollections().map(async (collection) => {
-      const obj = await generateCollectionObject(collection);
-      if (!obj) return;
-
-      return `\nexport const ${collection.name} = ${obj};`;
-    }),
-  );
-}
-
-async function generateDynamicIndexFile(ctx: FileGenContext) {
-  const { core, codegen, serverOptions, tc } = ctx;
-  const { configPath, environment, outDir } = core.getOptions();
-  // serializable config options
-  const partialOptions: CoreOptions = {
-    configPath,
-    environment,
-    outDir,
-  };
-  codegen.lines.push(
-    `import { dynamic } from 'fumadocs-mdx/runtime/dynamic';`,
-    `import * as Config from '${codegen.formatImportPath(configPath)}';`,
-    '',
-    `const create = await dynamic<typeof Config, ${tc}>(Config, ${JSON.stringify(partialOptions)}, ${JSON.stringify(serverOptions)});`,
-  );
-
-  async function generateCollectionObjectEntry(
-    collection: DocCollectionItem,
-    absolutePath: string,
-  ) {
-    const fullPath = path.relative(process.cwd(), absolutePath);
-    const content = await indexFileCache.read(fullPath).catch(() => '');
-    const parsed = fumaMatter(content);
-    const data = await core.transformFrontmatter(
-      {
-        collection,
-        filePath: fullPath,
-        source: content,
-      },
-      parsed.data as Record<string, unknown>,
-    );
-
-    const hash = createHash('md5').update(content).digest('hex');
-    const infoStr: string[] = [
-      // make sure it's included in vercel/nft
-      `absolutePath: path.resolve(${JSON.stringify(fullPath)})`,
-    ];
-    for (const [k, v] of Object.entries({
-      info: {
-        fullPath,
-        path: path.relative(collection.dir, absolutePath),
-      },
-      data,
-      hash,
-    } satisfies LazyEntry)) {
-      infoStr.push(`${k}: ${JSON.stringify(v)}`);
-    }
-
-    return `{ ${infoStr.join(', ')} }`;
-  }
-
-  async function generateCollectionObject(parent: CollectionItem): Promise<string | undefined> {
-    let collection: DocCollectionItem | undefined;
-    if (parent.type === 'doc') collection = parent;
-    else if (parent.type === 'docs') collection = parent.docs;
-
-    if (!collection || !collection.dynamic) return;
-
-    const files = await glob(collection.patterns, {
-      cwd: collection.dir,
-      absolute: true,
-    });
-    const entries = await Promise.all(
-      files.map((file) => generateCollectionObjectEntry(collection, file)),
-    );
-
-    switch (parent.type) {
-      case 'docs': {
-        const metaGlob = await generateMetaCollectionGlob(ctx, parent.meta, true);
-
-        return `await create.docs("${parent.name}", "${getBase(parent)}", ${metaGlob}, ${entries.join(', ')})`;
-      }
-      case 'doc':
-        return `await create.doc("${collection.name}", "${getBase(collection)}", ${entries.join(', ')})`;
     }
   }
 
